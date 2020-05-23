b95d07661d3f3cd0da537bd0d4294bd97e38d239
Aylin Aydın <aylin.aydin@ozu.edu.tr>
2020-05-24 01:00:53 +0300

    finialize button

--- src/Cs102_Proect02/OrderPanel.java
+++ src/Cs102_Proect02/OrderPanel.java
@@ -15,22 +15,27 @@ public class OrderPanel extends JPanel {
     public OrderPanel(Restaurant restaurant) {
         this.restaurant = restaurant;
 
-        this.mainPanel = new JPanel();
+       /* this.mainPanel = new JPanel();
         JButton newOrder =new JButton("New Order");
         this.mainPanel.add(newOrder);
-        add(mainPanel);
+        add(mainPanel);*/
 
         this.productAddPanel = new JPanel(new GridLayout(4,2));
         Border productBorder = BorderFactory.createTitledBorder("Add Product");
         this.productAddPanel.setBorder(productBorder);
+
         JLabel product = new JLabel("Product:");
+        product.setPreferredSize(new Dimension(300, 35));
         this.productAddPanel.add(product);
         JComboBox selectProduct = new JComboBox();
         addingProduct(selectProduct);
+        selectProduct.setPreferredSize(new Dimension(300, 35));
         this.productAddPanel.add(selectProduct);
         JLabel count = new JLabel("Count:");
+        count.setPreferredSize(new Dimension(300, 35));
         this.productAddPanel.add(count);
         JSpinner spinner = new JSpinner();
+        spinner.setPreferredSize(new Dimension(300, 35));
         this.productAddPanel.add(spinner);
         JLabel price = new JLabel("Price:");
         this.productAddPanel.add(price);
@@ -38,6 +43,7 @@ public class OrderPanel extends JPanel {
         this.productAddPanel.add(priceDisplay);
         JButton addButton = new JButton("Add");
         this.productAddPanel.add(addButton);
+
         DefaultTableModel model = new DefaultTableModel();
         model.addColumn("Product Name");
         model.addColumn("Count");
@@ -45,11 +51,18 @@ public class OrderPanel extends JPanel {
         add(productAddPanel);
 
         this.currentOrder = new JPanel();
+        Border orderBorder = BorderFactory.createTitledBorder("Current Order");
+        currentOrder.setBorder(orderBorder);
         JTable orderTable = new JTable(model);
         this.currentOrder.add(orderTable);
         orderTable.setPreferredScrollableViewportSize(new Dimension(600, 200));
         this.currentOrder.add(new JScrollPane(orderTable));
         add(currentOrder);
+
+        this.finalPanel = new JPanel();
+        JButton finalize = new JButton("Finalize");
+        this.finalPanel.add(finalize);
+        add(finalPanel);
 
 
 
