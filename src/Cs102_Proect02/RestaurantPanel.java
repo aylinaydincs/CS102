d4b689f84fa160d5dc71a2089d2216903c87d48c
Aylin Aydın <aylin.aydin@ozu.edu.tr>
2020-05-23 17:13:08 +0300

    Creating Restaurant Panel buttons

--- src/Cs102_Proect02/RestaurantPanel.java
+++ src/Cs102_Proect02/RestaurantPanel.java
@@ -1,6 +1,22 @@
 package Cs102_Proect02;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class RestaurantPanel extends JPanel {
+    private Restaurant restaurant;
+    protected JButton listEmployees ;
+    protected JButton addCook;
+    protected JButton addWaiter;
+    protected JButton calculateExpenses;
+
+    public RestaurantPanel(Restaurant restaurant) {
+        this.restaurant = restaurant;
+        this.setLayout(new FlowLayout());
+        this.listEmployees = new JButton("List Employees");
+        this.addCook = new JButton("Add Cook");
+        this.addWaiter = new JButton("Add Waiter");
+        this.calculateExpenses = new JButton("Calculate Expenses");
+
+    }
 }
